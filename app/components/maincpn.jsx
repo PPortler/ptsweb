dacb40b5a4da79fb7abec9f99dbbe9d90695aaae
pportlerkku <phitakpong.s@kkumail.com>
2024-06-29 04:38:30 +0700

    mainCPN 100% v1.0

--- app/components/maincpn.jsx
+++ app/components/maincpn.jsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 export default function Maincpn() {
 
     return (
-        <div className={`${style.maincpn} border-bottom border-black border-3
-        d-flex flex-column pt-md-4 pt-3 align-items-center`}>
+        <div className={`${style.maincpn} 
+        d-flex flex-column pt-4 align-items-center`}>
 
             {/* <div className={`${style.maincpn_draw_people}`}>
                 <div className={`${style.maincpn_draw_people_eye} ${style.maincpn_draw_people_eye_left}`}>
@@ -20,12 +20,12 @@ export default function Maincpn() {
             <div style={{ width: "max-content" }}>
                 <h1 className='container fs-lg-1'>ทำเว็บอะไรดี ?</h1>
                 <div className={`${style.maincpn_p_group} mt-3 bg-dark text-white p-2 p-sm-3 `} >
-                    <p className=' m-0 '>console.log(<span style={{color: "orange"}}>"รับเขียนเว็บไซต์ ตามความต้องการ"</span>);</p>
-                    <p className=' m-0'>console.log(<span style={{color: "orange"}}>"หรือ ตัดไฟล์ รูป photoshop, adobe, figma etc. เป็น website"</span>);</p>
+                    <p className=' m-0 '>console.log(<span style={{ color: "orange" }}>"รับเขียนเว็บไซต์ ตามความต้องการ"</span>);</p>
+                    <p className=' m-0'>console.log(<span style={{ color: "orange" }}>"หรือ ตัดไฟล์ รูป photoshop, adobe, figma etc. เป็น website"</span>);</p>
                 </div>
             </div>
 
-            <div className={`${style.maincpn_animate} d-sm-none d-flex justify-content-between mt-sm-5 mt-4`}>
+            <div className={`${style.maincpn_animate}  d-flex justify-content-between mt-sm-5 mt-4`}>
                 <Image src="/image/bird2.png" height={1000} width={1000} className={`${style.maincpn_animate_item}`}></Image>
             </div>
             {/* <div className={`${style.maincpn_cloud_group}`}>
@@ -34,11 +34,42 @@ export default function Maincpn() {
                 <Image src="/image/cloud.png" height={1000} width={1000} className={`${style.maincpn_cloud}`}></Image>
             </div> */}
 
-            
-           <div className={`${style.maincpn_model} d-flex mt-sm-5 mt-3 `}>
-                <Image src="/image/poster_main_1.png" height={1000} width={1000} className={`${style.maincpn_model_img} `}></Image>
-
-           </div>
+            <div id='maincpn_slider' className={`carousel slide ${style.maincpn_model} mt-sm-5 mt-3 `}>
+                <div className='carousel-inner'>
+                    <div className='carousel-item active'>
+                        <div>
+                            <h4 className='h3 text-center mb-3'>เว็บไซต์ โปรโมท</h4>
+                            <Image src="/image/poster_main_3.png" height={1000} width={1000} className={`${style.maincpn_model_img} `}></Image>
+                        </div>
+                    </div>
+                    <div className='carousel-item'>
+                        <div>
+                            <h4 className='h3 text-center mb-3'>เว็บไซต์ อ่านหนังสือ</h4>
+                            <Image src="/image/poster_main_1.png" height={1000} width={1000} className={`${style.maincpn_model_img} `}></Image>
+                        </div>
+                    </div>
+                    <div className='carousel-item'>
+                        <div>
+                        <h4 className='h3 text-center mb-3'>เว็บไซต์ Portfolio</h4>
+                            <Image src="/image/poster_main_2.png" height={1000} width={1000} className={`${style.maincpn_model_img} `}></Image>
+                        </div>
+                    </div>
+                </div>
+                <div className={`${style.maincpn_model_btn} fs-2 `}>
+                    <button className={`carousel-control-prev`} type='button' data-bs-target="#maincpn_slider" data-bs-slide="prev">
+                        <span className='bi bi-arrow-left-circle-fill' aria-hidden="true"></span>
+                        <span className='visually-hidden'></span>
+                    </button>
+                    <button className={`carousel-control-next`} type='button' data-bs-target="#maincpn_slider" data-bs-slide="next">
+                        <span className='bi bi-arrow-right-circle-fill' aria-hidden="true"></span>
+                        <span className='visually-hidden'></span>
+                    </button>
+                </div>
+                <div className={`${style.maincpn_model_fade}`}>
+                    <div className={``}></div>
+                    <div className={``}></div>
+                </div>
+            </div>
         </div>
     );
 }
